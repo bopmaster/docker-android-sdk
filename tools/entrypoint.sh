bc0db3f89c64302818450565e748daa942853523
bopmaster <boppro1992@yahoo.com>
2018-03-02 12:21:55 +0200

    Update entrypoint.sh

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -9,16 +9,4 @@ function su_mt_user() {
 }
 
 chown android:android /opt/android-sdk-linux
-
-if checkbin; then
-    exec su-exec android:android /opt/tools/android-sdk-update.sh "$@"
-else
-    su_mt_user /opt/tools/android-sdk-update.sh ${1}
-fi
-
-
-
-
-
-
-
+exec su-exec android:android /opt/tools/android-sdk-update.sh "$@"
